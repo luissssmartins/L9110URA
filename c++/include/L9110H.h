0795e4eaa6deb5c7b2844527732658aed1bd93ca
Luis Martins <chateadorn@gmail.com>
2023-03-07 22:59:08 -0300

    alterando métodos da ponte H

--- c++/include/L9110H.h
+++ c++/include/L9110H.h
@@ -11,13 +11,13 @@ class L9110H {
 
       void motor(int motorNumber, String instruction, int speed);
 
-      void forward(int i);
+      void forward(int i, int seconds, bool hasStop);
 
-      void backward(int i);
+      void backward(int i, int seconds, bool hasStop);
 
-      void right(int i);
+      void right(int i, int seconds, bool hasStop);
 
-      void left(int i);
+      void left(int i, int seconds, bool hasStop);
 
       void stop();
 
